e2ac668489db295eeec526870ff12e870574abd6
woozie1908 <erwinjung1995@gmail.com>
2025-09-28 21:07:17 -0700

    About to start human answers

--- RPS.js
+++ RPS.js
@@ -6,14 +6,21 @@ function getComputerChoice() {
         return "rock";
     } else if (randomNumber >= 0.33 && randomNumber <= 0.66) {
         return "paper"
-    } else if (randomNumber >= 0.66) {
+    } else if (randomNumber > 0.66) {
         return "scissors"
     }
-
-
 }
-    
 
+function getHumanChoice() {
+    let choice = prompt("Choose rock, paper, or scissors:");
+    return choice;
+}
+
+//1. Add Rock, Paper, Scissor for Human Choices
+//2. Add verses between computer and human
+//3. Add if win or lose
+    
+console.log("You chose: " + getHumanChoice());
 console.log(getComputerChoice());
 console.log(getComputerChoice());
 console.log(getComputerChoice());
